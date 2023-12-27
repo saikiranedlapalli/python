f614644f0a6086b3aeabed7978af34176ea02255
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 21:06:02 +0530

    Update Check Armstrong.py

--- Check Armstrong.py
+++ Check Armstrong.py
@@ -20,30 +20,6 @@ false
 
 """
 
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
 
 ## Read input as specified in the question.
 ## Print output as specified in the question.
