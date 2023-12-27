966a7742db1800e1343ef3ed7085d1dafe25f871
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 21:18:02 +0530

    Update Zeros and Stars Pattern.py

--- Zeros and Stars Pattern.py
+++ Zeros and Stars Pattern.py
@@ -26,30 +26,6 @@ Sample Output 2 :
 
 """
 
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
 
 ## Read input as specified in the question.
 ## Print output as specified in the question.
