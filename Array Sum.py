69eacf6f039e3ef4f1615c716a0da06512273f6d
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 20:53:40 +0530

    Update Array Sum.py

--- Array Sum.py
+++ Array Sum.py
@@ -20,26 +20,6 @@ from sys import *
 from collections import *
 from math import *
 
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
 ## Read input as specified in the question.
 ## Print output as specified in the question.
 n = int(input())
