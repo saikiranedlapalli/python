af41c226667d8833604aab0e7e776b20be876c1a
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 21:15:00 +0530

    Update Interesting Alphabets.py

--- Interesting Alphabets.py
+++ Interesting Alphabets.py
@@ -36,31 +36,6 @@ ABCDEFG
 
 """
 
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
 ## Read input as specified in the question.
 ## Print output as specified in the question.
 n = int(input())
