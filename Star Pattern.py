14021541fb5da7810215105d6558fd237b276cee
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 21:19:07 +0530

    Update Star Pattern.py

--- Star Pattern.py
+++ Star Pattern.py
@@ -31,31 +31,6 @@ Sample Output 2 :
 
 """
 
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
 ## Read input as specified in the question.
 ## Print output as specified in the question.
 n = int(input())
