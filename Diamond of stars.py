b06e9bc593561d3b36ef93db4fff26069d8f50c9
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 21:08:52 +0530

    Update Diamond of stars.py

--- Diamond of stars.py
+++ Diamond of stars.py
@@ -1,83 +1,4 @@
-"""from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-## Read input as specified in the question.
-## Print output as specified in the question.
-n = int(input())
-
-n = (n+1)//2
-i = 1
-
-while i <= n:
-    j = 1
-    p = 1
-    while j <= n:
-        if j >= n-i+1:
-            print('*', end = '')
-            p = p + 1
-        else:
-            print(' ', end = '')
-        j = j + 1
-        
-        while p - 2 >= 1:
-            print('*', end = '')
-            p = p - 1
-    i = i + 1
-    print()
-
-    
-i = i - 2
-
-while i >= 1:
-    j = 1
-    p = 1
-    while j <= n:
-        if j >= n-i+1:
-            print('*', end = '')
-            p = p + 1
-        else:
-            print(' ', end = '')
-        j = j + 1
-        
-        while p - 2 >= 1:
-            print('*', end = '')
-            p = p - 1
-    i = i - 1
-    print()
-Print the following pattern for the given number of rows.
-Note: N is always odd.
-
-
-Pattern for N = 5
-
-
-
-The dots represent spaces.
-
-
-
+"""
 Input format :
 N (Total no. of rows and can only be odd)
 Output format :
@@ -100,31 +21,6 @@ Sample Output 2:
   *
 
 """
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from os import *
-from sys import *
-from collections import *
-from math import *
 
 ## Read input as specified in the question.
 ## Print output as specified in the question.
