7ef19771f254af039c1c4d39214d5aee3c025b89
Saikiran Edlapalli <95921732+saikiranedlapalli@users.noreply.github.com>
2023-12-27 20:58:49 +0530

    Update Alpha Pattern.py

--- Alpha Pattern.py
+++ Alpha Pattern.py
@@ -32,10 +32,6 @@ EEEEE
 FFFFFF
 
 """
-from os import *
-from sys import *
-from collections import *
-from math import *
 
 ## Read input as specified in the question.
 ## Print output as specified in the question.
